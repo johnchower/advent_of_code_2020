5137641ddb2bfe60c72081114993cf31524252ee
John Hower <johnchower@gmail.com>
2020-12-13 14:52:45 -0600

    Make script more generlizeable

--- template.r
+++ template.r
@@ -1,3 +1,33 @@
-library(rprojroot)
+library(tidyverse)
 
-rprojroot::find_root(criterion = rprojroot::has_dir(".git"))
+project_directory <- rprojroot::find_root(
+  criterion = rprojroot::has_dir(".git")
+)
+
+day <- 1
+part <- 1
+day_part <- paste0("day", day, "_", "part", part)
+
+input <- read_csv(
+  paste0(
+    project_directory
+  , "/"
+  , "inputs/"
+  , day_part
+  , ".txt"
+  )
+, col_names = "entry"
+)
+
+answer <- 2
+
+write_lines(
+  answer
+, paste0(
+    project_directory
+  , "/"
+  , "answers/"
+  , day_part
+  , ".txt"
+  )
+)
