c779fe9052e08dcfc5e7bbd6a9ba48698c0ddaa0
John Hower <johnchower@gmail.com>
2020-12-13 14:25:59 -0600

    Solve part 1

--- day1.r
+++ day1.r
@@ -1,3 +1,15 @@
-library(rprojroot)
+library(tidyverse)
 
-rprojroot::find_root(criterion = rprojroot::has_dir(".git"))
+project_directory <- rprojroot::find_root(criterion = rprojroot::has_dir(".git"))
+setwd(project_directory)
+
+input <- read_csv("inputs/day1_part1.txt", col_names = "entry")
+
+input %>%
+  mutate(
+    complement = 2020 - entry
+  ) %>%
+  left_join(
+    input
+  , by = c("complement" = "entry")
+  )
