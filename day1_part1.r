8c91be8d5c1b59cda2b858b3fe000d4101f9121b
John Hower <johnchower@gmail.com>
2020-12-13 14:50:53 -0600

    Make script more generlizeable

--- day1_part1.r
+++ day1_part1.r
@@ -1,15 +1,46 @@
 library(tidyverse)
 
-project_directory <- rprojroot::find_root(criterion = rprojroot::has_dir(".git"))
-setwd(project_directory)
+project_directory <- rprojroot::find_root(
+  criterion = rprojroot::has_dir(".git")
+)
 
-input <- read_csv("inputs/day1_part1.txt", col_names = "entry")
+day <- 1
+part <- 1
+day_part <- paste0("day", day, "_", "part", part)
 
-input %>%
+input <- read_csv(
+  paste0(
+    project_directory
+  , "/"
+  , "inputs/"
+  , day_part
+  , ".txt"
+  )
+, col_names = "entry"
+)
+
+answer <- input %>%
   mutate(
     complement = 2020 - entry
   ) %>%
-  left_join(
+  inner_join(
     input
   , by = c("complement" = "entry")
+  ) %>%
+  mutate(
+    answer = complement * entry
+  ) %>%
+  distinct(answer) %>% {
+    .$answer
+  }
+
+write_lines(
+  answer
+, paste0(
+    project_directory
+  , "/"
+  , "answers/"
+  , day_part
+  , ".txt"
   )
+)
