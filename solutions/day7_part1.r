1e18b8cd9f93415da1d5040ab4b543049529a630
John Hower <johnchower@gmail.com>
2020-12-16 20:25:01 -0600

    Solve day7 part1

---
+++ solutions/day7_part1.r
@@ -0,0 +1,108 @@
+library(tidyverse)
+library(stringr)
+library(igraph)
+
+project_directory <- rprojroot::find_root(
+  criterion = rprojroot::has_dir(".git")
+)
+
+day <- 7
+part <- 1
+
+input <- read_lines(
+  paste0(
+    project_directory
+  , "/"
+  , "inputs/day"
+  , day
+  , ".txt"
+  )
+)
+
+test_input <- read_lines(
+  paste0(
+    project_directory
+  , "/"
+  , "test_inputs/day"
+  , day
+  , "_part"
+  , part
+  , ".txt"
+  )
+)
+
+input_clean <- input %>% {
+  tibble(rule = .) 
+} %>%
+  mutate(
+    rule = str_replace_all(rule, "bags?", "")
+  , rule = str_replace_all(rule, " .$", "")
+  ) %>%
+  separate(
+    rule
+  , into = c("container", "containees")
+  , sep = "  contain "
+  ) %>%
+  mutate(
+    container = str_trim(container)
+  , containees = str_split(containees, " , ")
+  , containees = map(
+      containees
+    , ~ tibble(containee = str_trim(.))
+    )
+  ) %>%
+  unnest() %>%
+  mutate(
+    data = str_split(containee, " ", n = 2)
+  , containee_count = map(data, ~ tibble(containee_count = .[1]))
+  , containee_color = map(data, ~ tibble(containee_color = .[2]))
+  ) %>% 
+  unnest(
+    containee_count, containee_color
+  ) %>%
+  select(- data, - containee) %>%
+  mutate(
+    containee_count = ifelse(
+      containee_count == "no"
+    , 0
+    , as.numeric(containee_count)
+    )
+  )
+
+edges <- input_clean %>%
+  filter(containee_count > 0) %>%
+  select(- containee_count)
+
+adjacency_matrix <- edges %>%
+  graph_from_data_frame() %>%
+  as_adj()
+
+exponent <- 1
+latest_power <- adjacency_matrix
+power_list <- list(latest_power)
+while (sum(latest_power[, "shiny gold"]) > 0){
+  exponent <- exponent + 1
+  latest_power <- adjacency_matrix %*% latest_power
+  power_list <- c(power_list, latest_power)
+}
+
+answer <- power_list %>%
+  map(~ .[, "shiny gold"]) %>%
+  map(~ .[. > 0]) %>%
+  map(names) %>%
+  unlist() %>%
+  unique() %>%
+  length()
+
+write_lines(
+  answer
+, paste0(
+    project_directory
+  , "/"
+  , "answers/day"
+  , day
+  , "_part"
+  , part
+  , ".txt"
+  )
+)
