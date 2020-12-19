9d748a0a07d8961bf66bf4527d0fefba6c1ff19e
John Hower <johnchower@gmail.com>
2020-12-19 13:11:11 -0600

    Solve day8 part2

--- solutions/day8_part2.r
+++ solutions/day8_part2.r
@@ -30,7 +30,7 @@ test_input <- read_lines(
   )
 )
 
-input_clean <- test_input %>%
+input_clean <- input %>%
   tibble(instruction = .) %>%
   separate(instruction, into = c("instruction_type", "value"), sep = " ") %>%
   mutate(
@@ -56,27 +56,55 @@ get_increments <- function(input_clean) {
   return(increments)
 }
 
-input_clean %>%
-  select(
-    id
-  , next_id
-  ) %>%
-  graph_from_data_frame() %>%
-  plot()
-
-accumulator <- 0
-current_id <- 1
-id_list <- c(current_id)
-while (length(unique(id_list)) == length(id_list)) {
-  next_instruction <- input_clean %>%
-    filter(id == current_id)
-  next_id <- next_instruction$next_id
-  accumulator <- accumulator + next_instruction$accumulator_increment
-  id_list <- c(id_list, next_id)
-  current_id <- next_id
+switch_instruction <- function(input_clean, id_to_switch) {
+  input_clean[id == id_to_switch, "instruction_type"]
+    mutate(
+      instruction_type = ifelse(
+        id != id_to_switch | instruction_type == "acc"
+      , instruction_type
+      , ifelse(
+          instruction_type == "nop"
+        , "jmp"
+        , "nop"
+        )
+      )
+    )
 }
 
-answer <- accumulator
+run_program <- function(altered_input) {
+  end_state <- max(altered_input$id) + 1
+  accumulator <- 0
+  current_id <- 1
+  id_list <- c(current_id)
+  while (
+    length(unique(id_list)) == length(id_list) & !(end_state %in% id_list)
+  ) {
+    next_instruction <- altered_input %>%
+      filter(id == current_id)
+    next_id <- next_instruction$next_id
+    accumulator <- accumulator + next_instruction$accumulator_increment
+    id_list <- c(id_list, next_id)
+    current_id <- next_id
+  }
+  tibble(
+    terminated = end_state %in% id_list
+  , accumulator_value = accumulator
+  )
+}
+
+start_time <- Sys.time()
+answer <- input_clean %>%
+  filter(instruction_type != "nop") %>%
+  {.$id} %>%
+  map(~ switch_instruction(input_clean, .)) %>%
+  map(get_increments) %>%
+  map(run_program) %>%
+  reduce(bind_rows) %>%
+  filter(terminated) %>% {
+    .$accumulator_value
+  }
+end_time <- Sys.time()
+end_time - start_time
 
 write_lines(
   answer
